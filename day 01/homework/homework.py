7264f1d86ec6afdbb7440aa088bc53493c856cc9
nemcki <iadzegiorgi98@gmail.com>
2024-10-09 20:39:44 +0400

    first commit

---
+++ day 01/homework/homework.py
@@ -0,0 +1,5 @@
+name="giorgi"
+surname= "iadze"
+age=26
+height=193.5 
+print("hello how are you my name is" + " " +name+ " "+surname+" " "i am"+" "+str(age)+" "+ "years old"+" "+ "my height is"+ " " +str(height)+"cm")
